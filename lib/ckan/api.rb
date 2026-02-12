0f89d5f23b62b83c98b5371d9243e36c2d42839e
Nicolas <>
2026-02-12 12:46:56 +0100

    feat: add CKAN v3 support

--- lib/ckan/api.rb
+++ lib/ckan/api.rb
@@ -1,11 +1,32 @@
 module CKAN
   class API
     @api_url = "http://ckan.net/api/1/"
+    @api_version = nil
+    @api_key = nil
+
     def self.api_url=(api_url)
+      @api_version ||= api_url[/api\/(\d+)\//, 1] || "1"
       @api_url = api_url
     end
+
     def self.api_url
       @api_url
+    end
+
+    def self.api_version=(version)
+      @api_version = version
+    end
+
+    def self.api_version
+      @api_version
+    end
+
+    def self.api_key=(api_key)
+      @api_key = api_key
+    end
+
+    def self.api_key
+      @api_key
     end
   end
 end
--- lib/ckan/model.rb
+++ lib/ckan/model.rb
@@ -4,8 +4,16 @@ module CKAN
 
     def read_lazy_data
       unless @lazy_data_read
-        self.class.read_remote_json_data(self.class.site + "/" + self.id).
-          each do |name,value|
+        data = if self.class.use_v3_api?
+          # v3 API: action/<model>_show?id=<id>
+          result = self.class.read_remote_json_data(self.class.v3_show_url(self.id))
+          result["result"]
+        else
+          # v1 API: rest/<model>/<id>
+          self.class.read_remote_json_data(self.class.site + "/" + self.id)
+        end
+
+        data.each do |name,value|
           self.instance_variable_set("@"+name,value)
         end
         @lazy_data_read = true
@@ -33,7 +41,13 @@ module CKAN
     end
 
     def self.read_remote_json_data(address)
-      JSON.parse(URI.open(address).read)
+      headers = {}
+      if CKAN::API.api_key
+        headers['Authorization'] = CKAN::API.api_key
+        headers['X-CKAN-API-Key'] = CKAN::API.api_key
+      end
+
+      JSON.parse(URI.open(address, headers).read)
     end
 
     def self.lazy_reader(*names)
@@ -43,6 +57,17 @@ module CKAN
           instance_variable_get("@" + name.to_s)
         end
       end
+    end
+
+    def self.use_v3_api?
+      CKAN::API.api_version.to_s == "3"
+    end
+
+    def self.v3_show_url(id)
+      # Override in subclasses if needed
+      # Default: action/<model_name>_show?id=<id>
+      model_name = self.name.split('::').last.downcase
+      base + "api/3/action/#{model_name}_show?id=#{id}"
     end
   end
 end
